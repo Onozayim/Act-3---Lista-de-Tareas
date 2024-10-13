79ea3176ed6449f0e46360c4cc2f79daf5f60595
Onozayim <camilomdo@gmail.com>
2024-10-13 17:55:01 -0600

    back connection

--- app.js
+++ app.js
@@ -1,97 +1,99 @@
 //define el campo donde se van a guardar las tareas, y las obtiene desde el local storage
 const task_key = "tasks";
-var task_array = JSON.parse(localStorage.getItem(task_key)) ?? [];
 
 //define los elemetnos del html que vamos a usar en el programa
 const task_form = document.getElementById("task_form");
 const task_input = document.getElementById("task_input");
 const task_list = document.getElementById("task_list");
 
+//url del back-end
+const url = "http://localhost:8000/api/task";
+
 //funcion que guarde las tareas
-const saveTask = (ev) => {
+const saveTask = async (ev) => {
   ev.preventDefault();
 
-  if (!task_input.value) {
-    alert("La tarea no puede estar vacía");
+  try {
+    const res = await axios.post(url + "/create", { task: task_input.value });
+
+    addTask(res.data.data);
+
+    task_input.value = "";
+    return;
+  } catch (error) {
+    alert(error.response.data.message);
     return;
   }
+};
 
-  //crea un nuevo objeto de la tarea, y la guarda en el local storage
-  const newTask = {
-    task: task_input.value,
-    done: false,
-  };
+const addTask = (task) => {
+  task_list.insertAdjacentHTML(
+    "afterend",
+    `<li
+          class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2"
+          id = "task-${task.id}"
+      >
+          <div id="task-text-${task.id}" class="d-flex align-items-center break-word">${
+            task.done ? showTaskCompleted(task) : task.task
+          }</div>
 
-  task_array.push(newTask);
-  localStorage.setItem(task_key, JSON.stringify(task_array));
-
-  task_input.value = null;
-
-  showTasks();
+          <div class="min-width">
+              ${
+                !task.done
+                  ? showCompleteButton(task.id, "", task.task)
+                  : showCompleteButton(task.id, "hidden", task.task)
+              }
+              <button class="btn btn-danger" onclick="deleteTask(${task.id})">
+                  <i class="bi-trash"></i>
+              </button>
+          </div>
+      </li>`
+  );
 };
 
 //Remueve el elemento del arreglo por el indice, y lo guarda en el local storage
-const deleteTask = (index) => {
-  task_array.splice(index, 1);
-  localStorage.setItem(task_key, JSON.stringify(task_array));
-
-  showTasks();
+const deleteTask = async (index) => {
+  try {
+    await axios.delete(url + "/delete/" + index);
+    document.getElementById(`task-${index}`).remove();
+  } catch (error) {
+    alert(error.response.data.message);
+    return;
+  }
 };
-
 
 //cambia el campo "done" de la tarea, y lo guarda en el local storage
-const completeTask = (index) => {
-  if (task_array[index].done) return;
-
-  task_array[index].done = true;
-  localStorage.setItem(task_key, JSON.stringify(task_array));
-
-  showTasks();
+const completeTask = async (index, task) => {
+  try {
+    await axios.patch(url + "/complete/" + index);
+    document.getElementById(`complete-${index}`).disabled = true;
+    document.getElementById(`task-text-${index}`).innerHTML = `<s>${task}</s>`
+  } catch (error) {
+    alert(error.response.data.message);
+    return;
+  }
 };
 
-//Regresa el elemento html de la tarea tachada 
+//Regresa el elemento html de la tarea tachada
 const showTaskCompleted = (task) => {
   return `<s>${task.task}</s>`;
 };
 
 //Regresa el boton html de completar tarea
-const showCompleteButton = (index, hide) => {
+const showCompleteButton = (index, hide, task) => {
   return `
-        <button class="btn btn-success ${hide}" onclick="completeTask(${index})">
+        <button id="complete-${index}" class="btn btn-success ${hide}" onclick="completeTask(${index}, '${task}')" ${hide ? "disabled": ""}>
             <i class="bi-check"></i>
         </button>`;
 };
 
-
 //Recopila las tareas y las agrega al elemnto ul del html
-const showTasks = () => {
-  let tasks = "";
+const showTasks = async () => {
+  const res = await axios.get(url);
 
-  task_array.forEach((element, index) => {
-    tasks += `
-        <li
-            class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2"
-        >
-            <div class="d-flex align-items-center break-word">${
-              element.done ? showTaskCompleted(element) : element.task
-            }</div>
-
-            <div class="min-width">
-                ${
-                  !element.done
-                    ? showCompleteButton(index, "")
-                    : showCompleteButton(index, "hidden")
-                }
-                <button class="btn btn-danger" onclick="deleteTask(${index})">
-                    <i class="bi-trash"></i>
-                </button>
-            </div>
-        </li>
-        
-        `;
+  res.data.data.forEach((element) => {
+    addTask(element);
   });
-
-  task_list.innerHTML = tasks;
 };
 
 showTasks();
